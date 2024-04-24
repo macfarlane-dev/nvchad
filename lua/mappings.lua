3d9980326079743119e8d96e3462ad287081e8eb
Alastair Macfarlane <alastair.macfarlane@gallagher.com>
2024-04-24 10:42:25 +1000

    feat(git): key mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,20 +1,31 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
+-- alternative entrance to command mode
 map("n", ";", ":", { desc = "CMD enter command mode" })
+
+-- alternative escape from interactive mode
 map("i", "jk", "<ESC>")
+
+-- search
 map("n", "<leader>fk", ":Telescope keymaps<CR>", { desc = "Telescope Find Keymaps" })
 map("n", "<leader>f\"", ":Telescope registers<CR>", { desc = "Telescope Find registers" })
-map("n", "<c-i>", ":ToggleTerm direction=float<CR>", { desc = "Toggle floating terminal" })
+
+-- terminal
+map("n", "<leader>tt", ":ToggleTerm direction=float<CR>", { desc = "Toggle floating terminal" })
+
+-- git
+map("n", "<leader>gl", ":Telescope git_commits<CR>", { desc = "Git Log" })
+
 map("n", "<leader>gs", ":Git<CR>", { desc = "Git Status" })
 map("n", "<leader>gd", ":Gvdiff<CR>", { desc = "Git Diff" })
 map("n", "<leader>gD", ":Gvdiff ", { desc = "Git Diff another branch" })
 map("n", "<leader>ga", ":Gwrite<CR>", { desc = "Git Add" })
 map("n", "<leader>gR", ":Gread<CR>", { desc = "Git Reset" })
 map("n", "<leader>gC", ":Git commit<CR>", { desc = "Git Commit" })
-map("n", "<leader>gl", ":Telescope git_commits<CR>", { desc = "Git Log" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "<leader>ghs", ":Gitsigns stage_hunk<CR>", { desc = "Git Hunk Stage" })
+map("n", "<leader>ghr", ":Gitsigns reset_hunk<CR>", { desc = "Git Hunk Reset" })
+map("n", "<leader>gB", ":Gitsigns toggle_current_line_blame", { desc = "Git toggle line Blame" })
+map("n", "<leader>go", ":Gitsigns toggle_signs", { desc = "Toggle On/Off git signs" })
