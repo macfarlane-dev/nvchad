1893989933844f087a091ad274c8d04a34ae4924
Alastair Macfarlane <macfarlane.dev@gmail.com>
2024-07-23 08:26:48 +1000

    fix(treesj): terraform uses python config

--- lua/configs/treesj.lua
+++ lua/configs/treesj.lua
@@ -1,8 +1,10 @@
 local tsj_utils = require('treesj.langs.utils')
 local java = require('treesj.langs.java')
+local python = require('treesj.langs.python')
 
 return {
   langs = {
-      groovy = tsj_utils.merge_preset(java, {})
+    groovy = tsj_utils.merge_preset(java, {}),
+    terraform = tsj_utils.merge_preset(python, {}),
   }
 }
