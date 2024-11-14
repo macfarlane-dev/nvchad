26425f516aaa4341e38dc089159b24ce6b1ccde2
Alastair Macfarlane <macfarlane.dev@gmail.com>
2024-11-14 15:11:26 +1100

    fix: describe multicursor keymappings

--- lua/configs/multicursor.lua
+++ lua/configs/multicursor.lua
@@ -20,19 +20,19 @@ M.setup = function()
   end, { desc = "Clear multiple cursors or highlights" })
 
   -- Add or skip cursor above/below the main cursor.
-  map({"n", "v"}, "<up>", function() mc.lineAddCursor(-1) end)
-  map({"n", "v"}, "<down>", function() mc.lineAddCursor(1) end)
-  map({"n", "v"}, "<leader><up>", function() mc.lineSkipCursor(-1) end)
-  map({"n", "v"}, "<leader><down>", function() mc.lineSkipCursor(1) end)
+  map({"n", "v"}, "<up>", function() mc.lineAddCursor(-1) end, { desc = "Add a multicursor and move up" })
+  map({"n", "v"}, "<down>", function() mc.lineAddCursor(1) end, { desc = "Add a multicursor and move down" })
+  map({"n", "v"}, "<leader><up>", function() mc.lineSkipCursor(-1) end, { desc = "Skip the current line and add a multicursor above" })
+  map({"n", "v"}, "<leader><down>", function() mc.lineSkipCursor(1) end, { desc = "Skip the current line and add a multicursor below" })
 
   -- Add or skip adding a new cursor by matching word/selection
-  map({"n", "v"}, "<leader>n", function() mc.matchAddCursor(1) end)
-  map({"n", "v"}, "<leader>s", function() mc.matchSkipCursor(1) end)
-  map({"n", "v"}, "<leader>N", function() mc.matchAddCursor(-1) end)
-  map({"n", "v"}, "<leader>S", function() mc.matchSkipCursor(-1) end)
+  map({"n", "v"}, "<leader>n", function() mc.matchAddCursor(1) end, { desc = "Add a multicursor and move to the next word" })
+  map({"n", "v"}, "<leader>s", function() mc.matchSkipCursor(1) end, { desc = "Skip this multicursor and move to the next word" })
+  map({"n", "v"}, "<leader>N", function() mc.matchAddCursor(-1) end, { desc = "Add a multicursor and move to the previous word" })
+  map({"n", "v"}, "<leader>S", function() mc.matchSkipCursor(-1) end, { desc = "Skip this multicursor and move to the previous word" })
 
   -- Add all matches in the document
-  map({"n", "v"}, "<leader>A", mc.matchAllAddCursors)
+  map({"n", "v"}, "<leader>A", mc.matchAllAddCursors, { desc = "Add a multicursor to all occurrences of the current word" })
 
   -- You can also add cursors with any motion you prefer:
   -- map("n", "<right>", function()
@@ -43,46 +43,46 @@ M.setup = function()
   -- end)
 
   -- Rotate the main cursor.
-  map({"n", "v"}, "<left>", mc.nextCursor)
-  map({"n", "v"}, "<right>", mc.prevCursor)
+  map({"n", "v"}, "<left>", mc.prevCursor, { desc = "Rotate the main cursor up multicuror lines" })
+  map({"n", "v"}, "<right>", mc.nextCursor, { desc = "Rotate the main cursor down multicuror lines" })
 
   -- Delete the main cursor.
-  map({"n", "v"}, "<leader>c", mc.deleteCursor)
+  map({"n", "v"}, "<leader>c", mc.deleteCursor, { desc = "Clear the main cursor" })
 
   -- Add and remove cursors with control + left click.
-  map("n", "<c-leftmouse>", mc.handleMouse)
+  map("n", "<c-leftmouse>", mc.handleMouse, { desc = "Add a Multicursor" })
 
   -- Easy way to add and remove cursors using the main cursor.
-  map({"n", "v"}, "<c-q>", mc.toggleCursor)
+  map({"n", "v"}, "<c-m>", mc.toggleCursor, { desc = "Add a Multicursor" })
 
   -- Clone every cursor and disable the originals.
-  map({"n", "v"}, "<leader><c-q>", mc.duplicateCursors)
+  map({"n", "v"}, "<leader><c-m>", mc.duplicateCursors, { desc = "Clone ever cursor and disable originals" })
 
   -- bring back cursors if you accidentally clear them
-  map("n", "<leader>gv", mc.restoreCursors)
+  map("n", "<leader>gv", mc.restoreCursors, { desc = "Restore cleared cursors" })
 
   -- Align cursor columns.
-  map("n", "<leader>a", mc.alignCursors)
+  map("n", "<leader>a", mc.alignCursors, { desc = "Align cursor columns" })
 
   -- Split visual selections by regex.
-  map("v", "S", mc.splitCursors)
+  map("v", "S", mc.splitCursors, { desc = "Split visual selection by regex" })
 
   -- Append/insert for each line of visual selections.
-  map("v", "I", mc.insertVisual)
-  map("v", "A", mc.appendVisual)
+  map("v", "I", mc.insertVisual, { desc = "Insert for each line of a visual selection" })
+  map("v", "A", mc.appendVisual, { desc = "Append for each line of a visual selection" })
 
   -- match new cursors within visual selections by regex.
-  map("v", "M", mc.matchCursors)
+  map("v", "M", mc.matchCursors, { desc = "" })
 
   -- Rotate visual selection contents.
   map("v", "<leader>t",
-      function() mc.transposeCursors(1) end)
+      function() mc.transposeCursors(1) end, { desc = "Rotate visual selection" })
   map("v", "<leader>T",
-      function() mc.transposeCursors(-1) end)
+      function() mc.transposeCursors(-1) end, { desc = "Rotate visual selection in reverse" })
 
   -- Jumplist support
-  map({"v", "n"}, "<c-i>", mc.jumpForward)
-  map({"v", "n"}, "<c-o>", mc.jumpBackward)
+  map({"v", "n"}, "<c-i>", mc.jumpForward, { desc = "Multicursor jump forward" })
+  map({"v", "n"}, "<c-o>", mc.jumpBackward, { desc = "Multicursor jump backward" })
 
   -- Customize how cursors look.
   local hl = vim.api.nvim_set_hl
