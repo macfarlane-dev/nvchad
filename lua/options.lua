8ab5c6d7e81a9feed8d135f7df8dccfad55077e7
Alastair Macfarlane <macfarlane.dev@gmail.com>
2025-01-29 18:33:19 +1100

    fix: enable folding

--- lua/options.lua
+++ lua/options.lua
@@ -5,6 +5,11 @@ local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 o.autochdir = false
 o.colorcolumn = "80"
+-- folding
+o.foldmethod = "indent"
+o.foldenable = true
+o.foldlevel = 99
+o.foldlevelstart = 99
 
 -- WORKAROUND FOR ISSUES RUNNING GIT BASH FOR WINDOWS
 -- only unix shells on windows will end in sh.exe
@@ -12,3 +17,4 @@ o.colorcolumn = "80"
 if string.match(vim.opt.shell._value, "sh.exe") then
   vim.opt.shellcmdflag = "-c"
 end
+
