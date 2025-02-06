0e35130ed44abbceb6c0579aade60dbffe887f16
Alastair Macfarlane <macfarlane.dev@gmail.com>
2025-02-07 08:52:44 +1100

    feat: init no-neck-pain.nvim

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "shortcuts/no-neck-pain.nvim",
+    lazy = false,
+    mappings = {
+      enabled = false,
+      toggle = "<Leader>np",
+      toggleLeftSide = "<Leader>nql",
+      toggleRightSide = "<Leader>nqr",
+      widthUp = "<Leader>n=",
+      widthDown = "<Leader>n-",
+      scratchPad = "<Leader>ns",
+    },
+    version = "*",
+  }
+}
