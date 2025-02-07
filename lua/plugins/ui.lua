8fdc32f39bc8bfda353c4764cf2c07f7e4071b8f
Alastair Macfarlane <macfarlane.dev@gmail.com>
2025-02-07 12:12:34 +1100

    fix: merge navigation and ui config files

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,5 +1,28 @@
 return {
   {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end
+    },
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = function(_, opts)
+      opts.filters = {
+        custom = {
+          "^node_modules$",
+          "^.git$",
+          "^.github$",
+        },
+        dotfiles = false,
+      }
+    end,
+  },
+  {
     "shortcuts/no-neck-pain.nvim",
     version = "*",
     lazy = false,
